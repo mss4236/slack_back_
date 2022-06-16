85efe8ff1432cb53dbc09352cfdb7027c5bb689a
KYEONGMIN PARK <mss4236@naver.com>
2022-06-17 03:34:59 +0900

    nestjs/config (dotenv?)

--- a-nest/src/app.controller.ts
+++ a-nest/src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+// service에서 실행한 결과값만 받아서 실행 ,,,, controller는 req, res 앎
+@Controller('api')  // /api
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get('user')  // GET /api/user
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getUser();
+  }
+
+  @Post('user')  // POST /api/user
+  postHello(): string {
+    return this.appService.postUser();
   }
 }
--- a-nest/src/app.service.ts
+++ a-nest/src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
+// 실제 동작은 service에서 (트랜젝션 단위로 만들어주면 좋음)
+// service는 req, res 모름(독립적으로 사용가능, service에서 req, res 쓰면 나누는 이유가 없음)
+// req, rest를 알면 test를 할때 mocking(외부에 의존하는 부분을 임의의로 가짜로 대체) 해줘야함
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return '논노노노노노논!';
+  constructor(private readonly configService: ConfigService) {}
+  getUser(): string {
+    return `getUser process.env.SECRET: ${process.env.SECRET} // getUser this.configService.get('SECRET'): ${this.configService.get('SECRET')}`;
+  }
+
+  postUser(): string {
+    return 'postUser Success';
   }
 }
